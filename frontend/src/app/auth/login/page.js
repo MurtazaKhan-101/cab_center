95b4bf053ac9f797cd6d501a3f459ae8d735a106
MhuzaifaWebDev <mhuzaifawebdev@gmail.com>
2025-11-20 17:56:35 +0500

    User Side

--- frontend/src/app/auth/login/page.js
+++ frontend/src/app/auth/login/page.js
@@ -1,13 +1,5 @@
-import { AuthLayout } from "../../components/layout/AuthLayout";
 import Login from "../../components/authentication/login";
 
 export default function LoginPage() {
-  return (
-    <AuthLayout
-      title="Welcome back"
-      subtitle="Log in to continue to Auth Template"
-    >
-      <Login />
-    </AuthLayout>
-  );
+  return <Login />;
 }
